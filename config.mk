aad3fe5a25f85c369bba6ba43f344c04f2613db6
Claudio Alessi <smoppy@gmail.com>
2025-11-23 00:00:21 +0100

    Add Makefile and LICENSE.

---
+++ config.mk
@@ -0,0 +1,14 @@
+# edo
+VERSION = 0.1
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+# flags
+CPPFLAGS = -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
+CFLAGS   = -std=c99 -g -pedantic -Wall -O0 ${CPPFLAGS}
+#CFLAGS  = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
+
+# compiler and linker
+CC = cc
